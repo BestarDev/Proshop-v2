ce606a46202aa718991090f53af67bce9b7f81f6
BestarDev <superstar9772@outlook.com>
2024-02-18 07:21:18 +0700

    LogOut & Register Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import {generateToken} from '../utils/generateToken.js';
 
 // @ desc       Auth User
 // @ route      POST /api/users/login
@@ -11,16 +12,7 @@ const authUser = asyncHandler(async(req, res) => {
     const user = await User.findOne({email})
 
     if(user && (await user.matchPassword(password))){
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
-            expiresIn: '30d'
-        })
-    
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000
-        })
+        generateToken(res, user._id);
 
         res.json({
             _id: user._id,
@@ -38,15 +30,44 @@ const authUser = asyncHandler(async(req, res) => {
 // @ route      POST /api/users
 // @ access     Public
 const registerUser = asyncHandler(async(req, res) => {
-    res.json("Register User");
+    const { name, email, password } = req.body;
+    const existUser = await User.findOne({email})
+
+    if(existUser) {
+        res.status(400);
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name, email, password
+    })
+
+    if(user) {
+        generateToken(res, user._id)
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid User Data');
+    }
 })
 
 // @ desc       Logout User
 // @ route      POST /api/users/logout
 // @ access     Public
-const logoutUser = asyncHandler(async(req, res) => {
-    res.json("Logout User");
-})
+const logoutUser = (req, res) => {
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+
+    res.status(200).json({ message: 'Log out successfully' })
+}
 
 // @ desc       Get User Profile
 // @ route      Get /api/users/profile

ed1ced0e3bade63aad19efda0ca4fc60d04e7466
BestarDev <144424811+BestarDev@users.noreply.github.com>
2024-02-21 19:01:56 +0700

    Update for Vite Project (not needed for this project)

---
+++ frontend/eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    // turn this one off
+    'react/prop-types': 'off',
+    // change these errors to warnings
+    'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': 'warn',
+  },
+};

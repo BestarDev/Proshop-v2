4791d9855eb86c3b0eef51480c651628679c13cb
BestarDev <superstar9772@outlook.com>
2024-02-21 17:54:21 +0700

    Finally Build

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler(async(req, res) => {
-    const pageSize = 8;
+    const pageSize = process.env.PAGINATION_LIMIT;
     const page = Number(req.query.pageNumber) || 1;
     const keyword = req.query.keyword ? {name: { $regex: req.query.keyword, $options: 'i'}} : {}
     const count = await Product.countDocuments({...keyword});
